73dc1ddf404c5861725a11817323d4543c54d119
alseamedia <alfred.chen@seamedia.com.tw>
2022-10-07 03:42:32 -0700

    init

---
+++ spec/aebet-real-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AebetRealView from '../lib/aebet-real-view';
+
+describe('AebetRealView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
